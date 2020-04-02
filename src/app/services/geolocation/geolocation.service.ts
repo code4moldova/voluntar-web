0313f6f6d89c951307085ad2ac8559d2959fd288
Valerii Fazli <valerii.fazli@upstartcommerce.com>
2020-04-02 15:44:20 +0300

    refactored httpParams for geoService

--- src/app/services/geolocation/geolocation.service.ts
+++ src/app/services/geolocation/geolocation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IAddresses } from '@models/geolocation';
 import { environment } from 'src/environments/environment';
@@ -9,20 +9,16 @@ import { environment } from 'src/environments/environment';
 })
 export class GeolocationService {
   url =
-    'https://info.iharta.md:6443/arcgis/rest/services/locator/CompositeLoc/GeocodeServer/findAddressCandidates?';
+    'https://info.iharta.md:6443/arcgis/rest/services/locator/CompositeLoc/GeocodeServer/findAddressCandidates';
 
   constructor(private http: HttpClient) {}
 
   getLocation(city: string, address: string): Observable<IAddresses> {
-    const url =
-      this.url +
-      'City=' +
-      encodeURI(city) +
-      '&SingleLine=' +
-      encodeURI(address) +
-      '&maxLocations=10&f=pjson';
+    let params = new HttpParams();
+    params = params.set('SingleLine', address);
+    params = params.set('f', 'pjson');
 
-    return this.http.get<IAddresses>(url);
+    return this.http.get<IAddresses>(this.url, { params });
   }
 
   getZones(): Observable<any> {
