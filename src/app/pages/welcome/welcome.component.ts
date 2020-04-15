48c12ba0b34cf6ccc938bd9fdd44a01aa8034a19
Grigore Fiodorov <grigore.fiodorov@gmail.com>
2020-04-15 17:26:08 +0300

    removed logo added change lang on top of page

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -7,13 +7,27 @@ import {TranslateService} from '@ngx-translate/core';
   styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
+
+  KEY_STORAGE_LANG = 'lang';
+
   constructor(public translate: TranslateService) {
     translate.addLangs(['ru', 'ro']);
     translate.setDefaultLang('ro');
-
     const browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/ro|ru/) ? browserLang : 'ro');
+    let storage_lang = localStorage.getItem(this.KEY_STORAGE_LANG);
+    if (storage_lang) {
+      translate.use(storage_lang);
+    } else {
+      translate.use(browserLang.match(/ro|ru/) ? browserLang : 'ro');
+    }
   }
+
+  changeLang(): void {
+    let new_lang = this.translate.currentLang === 'ru' ? 'ro' : 'ru';
+    this.translate.use(new_lang);
+    localStorage.setItem(this.KEY_STORAGE_LANG, new_lang);
+  }
+
   ngOnInit(): void {
   }
 }
