7c8abc4eb10de26a504cb290c86ce05e4a79f544
Andrew Luca <thendrluca@gmail.com>
2020-08-14 18:12:03 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // const corsHack = 'https://cors-anywhere.herokuapp.com/';
 export const environment = {
   production: true,
-  url: 'https://api.voluntar.md',
+  url: 'http://185.181.229.182:5000',
 };
