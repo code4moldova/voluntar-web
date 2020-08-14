1ffe220536dd28d66a65bf19929e3a960c2e41f6
Andrew Luca <thendrluca@gmail.com>
2020-08-14 18:16:16 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // const corsHack = 'https://cors-anywhere.herokuapp.com/';
 export const environment = {
   production: true,
-  url: 'http://185.181.229.182:5000',
+  url: 'https://185.181.229.182:5000',
 };
