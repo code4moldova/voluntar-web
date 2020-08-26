9ff8e0b2f6da0c6bc4f455fd279b70c92c2ec284
Andrew Luca <thendrluca@gmail.com>
2020-08-26 20:18:32 +0300

    chore: add deploy instructions

---
+++ scripts/generate-redirects.js
@@ -0,0 +1,18 @@
+const { writeFileSync } = require('fs')
+
+// https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables
+console.log('Generating "./src/_redirects" contents for:')
+console.log('CONTEXT:', process.env.CONTEXT) // e.g. deploy-preview
+console.log('BRANCH:', process.env.BRANCH) // e.g. pull/107/head
+console.log('HEAD:', process.env.HEAD) // e.g. genesis
+
+const domain = process.env.CONTEXT === 'production'
+  ? 'http://185.181.229.182:5000'
+  : 'http://185.181.229.182:5500'
+
+const _redirects = `
+/api/*  ${domain}/api/:splat  200
+/*      /index.html   200
+`
+
+writeFileSync('./src/_redirects', _redirects.trimLeft())
