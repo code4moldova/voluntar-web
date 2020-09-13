4155b34ad8e7ee51ad5d0bceb4c55a37800acd04
Andrew Luca <thendrluca@gmail.com>
2020-09-13 23:05:07 +0300

    feat(components): put all pages into a container with max width
    Closes #121

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
 import { AdminComponent } from './admin.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { MaterialComponentsModule } from 'src/app/shared/material.module';
@@ -8,6 +9,11 @@ import { HeaderComponent } from 'src/app/layout/header/header.component';
 
 @NgModule({
   declarations: [AdminComponent, HeaderComponent, SidebarComponent],
-  imports: [CommonModule, AdminRoutingModule, MaterialComponentsModule],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    MaterialComponentsModule,
+    SharedModule,
+  ],
 })
 export class AdminModule {}
