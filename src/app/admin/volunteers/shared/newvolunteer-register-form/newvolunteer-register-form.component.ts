3a1b6582bde6cf7691998ced6a1a9e90f72ef7ae
BaxLi <lilian.baxan@gmail.com>
2020-12-09 09:53:26 +0200

    multi-select adjusted ok

--- src/app/admin/volunteers/shared/newvolunteer-register-form/newvolunteer-register-form.component.ts
+++ src/app/admin/volunteers/shared/newvolunteer-register-form/newvolunteer-register-form.component.ts
@@ -1,86 +1,79 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import {
-  KIV_ZONES,
-  VOLUNTEER_ROLES,
-  VOLUNTEER_ROLES_ICONS,
-} from '@app/shared/constants';
-import { of } from 'rxjs';
-import { VolunteersService } from '../../volunteers.service';
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { KIV_ZONES, VOLUNTEER_ROLES, VOLUNTEER_ROLES_ICONS } from '@app/shared/constants'
+import { of } from 'rxjs'
+import { VolunteersService } from '../../volunteers.service'
 
 export interface NewRegistrationFormFields {
-  header: string;
-  controlName: string;
-  type?: string;
-  pattern?: string;
-  errorMessage?: string;
+  header: string
+  controlName: string
+  type?: string
+  pattern?: string
+  errorMessage?: string
 }
 
 @Component({
   templateUrl: './newvolunteer-register-form.component.html',
-  styleUrls: ['./newvolunteer-register-form.component.scss'],
+  styleUrls: ['./newvolunteer-register-form.component.scss']
 })
 export class NewVolunteerRegisterFormComponent implements OnInit {
-  form: FormGroup;
+  form: FormGroup
   formFields: Array<NewRegistrationFormFields> = [
     {
       header: 'Nume (Familie)',
       controlName: 'family',
-      errorMessage: 'atenție, eroare!',
+      errorMessage: 'atenție, eroare!'
     },
     { header: 'Prenume', controlName: 'name' },
     {
       header: 'Număr Telefon',
       controlName: 'phone',
-      errorMessage: '8 cifre vă rugăm',
+      errorMessage: '8 cifre vă rugăm'
     },
     {
       header: 'Email',
       controlName: 'email',
-      errorMessage: 'respectați formatul email',
+      errorMessage: 'respectați formatul email'
     },
     { header: 'Sector', controlName: 'zone' },
     { header: 'Adresa', controlName: 'address' },
     {
       header: 'Vîrsta',
       controlName: 'age',
-      errorMessage: ' Doar cifrele cu valorile > 18 sunt acceptate',
+      errorMessage: ' Doar cifrele cu valorile > 18 sunt acceptate'
     },
     {
       header: 'Profil Rețea Socializare',
       controlName: 'soc_media',
-      errorMessage: 'atenție, eroare!',
-    },
-  ];
+      errorMessage: 'atenție, eroare!'
+    }
+  ]
 
-  roles = VOLUNTEER_ROLES;
-  volunteerRolesIncons = VOLUNTEER_ROLES_ICONS;
-  sectors = KIV_ZONES;
+  roles = VOLUNTEER_ROLES
+  volunteerRolesIncons = VOLUNTEER_ROLES_ICONS
+  sectors = KIV_ZONES
 
   constructor(private volunteersService: VolunteersService) {}
-  onSubmit() {}
+  onSubmit() {
+    console.log('onSubmit pressed', this.form.get('role').value)
+  }
 
   ngOnInit(): void {
     this.form = new FormGroup({
       family: new FormControl('', [Validators.required]),
       name: new FormControl('', [Validators.required]),
-      phone: new FormControl('', [
-        Validators.required,
-        Validators.pattern(/^([0-9]){8}$/),
-      ]),
+      phone: new FormControl('', [Validators.required, Validators.pattern(/^([0-9]){8}$/)]),
       email: new FormControl('', [Validators.required, Validators.email]),
       zone: new FormControl('', [Validators.required]),
       address: new FormControl('', [Validators.required]),
-      age: new FormControl('', [
-        Validators.required,
-        Validators.pattern(/^([0-9]){2}$/),
-      ]),
+      age: new FormControl('', [Validators.required, Validators.pattern(/^([0-9]){2}$/)]),
       soc_media: new FormControl('', [Validators.required]),
+      //TODO - check -  maybe it take sense to provide TYPE of role form as VOLUNTEER_ROLES ???
       role: new FormControl(null, [Validators.required]),
       status: new FormControl(null, [Validators.required]),
       availability_days: new FormControl(null, [Validators.required]),
-      availability_hours: new FormControl(null, [Validators.required]),
-    });
+      availability_hours: new FormControl(null, [Validators.required])
+    })
   }
 
   closeDialog() {}
