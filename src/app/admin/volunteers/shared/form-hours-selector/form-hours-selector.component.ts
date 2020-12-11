513b850d25eeee203a85cd547dc8725d5ffca03a
DanielBaxan <daniel.baxan@gmail.com>
2020-12-11 12:08:24 +0200

    Updated hoursSelectComponent

--- src/app/admin/volunteers/shared/form-hours-selector/form-hours-selector.component.ts
+++ src/app/admin/volunteers/shared/form-hours-selector/form-hours-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { MatDialogRef } from '@angular/material/dialog'
 
 @Component({
@@ -10,14 +11,37 @@ export class FormHoursSelectorComponent implements OnInit {
   @Input('fn') fn: any
   @Output('onStartChange') onStartChange: EventEmitter<string> = new EventEmitter<string>()
   // @Output() onEndChange:EventEmitter<string>=new EventEmitter<string>()
-  start = '00-00'
-  end = '10-00'
+  start = '18-00'
+  end = '20-00'
+  selectHours = false
+  form: FormGroup
+  hours = [
+    '08:00',
+    '09:00',
+    '10:00',
+    '11:00',
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00'
+  ]
   constructor(private activeModal: MatDialogRef<any>) {
     console.log()
   }
 
   ngOnInit(): void {
     console.log('modal init')
+    this.form = new FormGroup({
+      availability_hours_start: new FormControl(null),
+      availability_hours_end: new FormControl(null)
+    })
 
     // this.start= fn.get('availability_hours_start').value
     // this.start= fn.get('availability_hours_end').value
@@ -31,5 +55,17 @@ export class FormHoursSelectorComponent implements OnInit {
     console.log('Emitted from modal')
     this.onStartChange.emit('++-++')
     // this.activeModal.close('Notify click');
+  }
+
+  openSelectHours() {
+    this.selectHours = !this.selectHours
+  }
+
+  onChangeStartHour(hour) {
+    this.start = hour
+  }
+
+  onChangeEndHour(hour) {
+    this.end = hour
   }
 }
