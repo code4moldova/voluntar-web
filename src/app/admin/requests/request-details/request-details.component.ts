fdde14955f93ccf14d90aacf430fcc75184efb16
DanielBaxan <56045886+DanielBaxan@users.noreply.github.com>
2020-12-15 17:28:02 +0200

    Dev daniel baxan (#5)
    * Rebase from main dev branch
    
    * Updated request form
    
    * Form maket done
    
    Co-authored-by: BaxLi <lilian.baxan@gmail.com>

--- src/app/admin/requests/request-details/request-details.component.ts
+++ src/app/admin/requests/request-details/request-details.component.ts
@@ -1,5 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
+
+import { SPECIAL_CONDITIONS, ZONES } from '@app/shared/constants'
+=======
 import { ZONES } from '@app/shared/constants'
 import { RequestsFacade } from '../requests.facade'
 
@@ -11,21 +14,75 @@ import { RequestsFacade } from '../requests.facade'
 export class RequestDetailsComponent implements OnInit, OnDestroy {
   form: FormGroup
   zones: Array<string> = Object.keys(ZONES).filter((key) => isNaN(+key))
+
+  ilnessOptions: any = [
+    {
+      label: 'None',
+      value: 'Nu are'
+    },
+    {
+      label: 'Has symptoms',
+      value: 'Are simptome'
+    }
+  ]
+  needs: any = [
+    {
+      label: 'Food',
+      value: 'Produse Alimentare'
+    },
+    {
+      label: 'Drugs',
+      value: 'Medicamente'
+    },
+    {
+      label: 'Transport',
+      value: 'Transport Persoana'
+    },
+    {
+      label: 'Payment',
+      value: 'Achitare Facturi'
+    }
+  ]
+  specialConditions = SPECIAL_CONDITIONS
+
   ilnessOptions: ['None', 'Simptoms']
   specialConditions: ['None', 'Disability', 'Blind', 'Deaf']
   needs: ['Food', 'Drugs', 'Transport', 'Payment']
+
   constructor(private requestsFacade: RequestsFacade) {}
 
   onSubmit(ev: Event) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
+
+      first_name: new FormControl(null, [Validators.required]),
+      last_name: new FormControl(null, [Validators.required]),
+      age: new FormControl(null),
+      zone: new FormControl(null, [Validators.required]),
+      address: new FormControl(null, [Validators.required]),
+      apartment: new FormControl(null),
+      entrance: new FormControl(null),
+      floor: new FormControl(null),
+      phone: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.maxLength(8)]),
+      landline: new FormControl(
+        null,
+        // because we add prefix 22 at form submit.
+        [Validators.required, Validators.minLength(6), Validators.maxLength(6)]
+      ),
+      special_condition: new FormControl(null, [Validators.required]),
+      ilness: new FormControl('', [Validators.required]),
+      need: new FormControl('', [Validators.required]),
+      comments: new FormControl(''),
+      password: new FormControl(null, [Validators.required])
+
       first_name: new FormControl('', [Validators.required]),
       last_name: new FormControl('', [Validators.required]),
       ilness: new FormControl('', [Validators.required]),
       zone: new FormControl('', [Validators.required]),
       special_conditions: new FormControl('', [Validators.required]),
       need: new FormControl('', [Validators.required])
+
     })
   }
   ngOnDestroy() {}
