2e23c4db636f6e15551997367217cddfc066be1f
DanielBaxan <daniel.baxan@gmail.com>
2020-12-16 11:51:21 +0200

    Updated request-details

--- src/app/admin/requests/request-details/request-details.component.ts
+++ src/app/admin/requests/request-details/request-details.component.ts
@@ -3,6 +3,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { SPECIAL_CONDITIONS, ZONES } from '@app/shared/constants'
 import { RequestsFacade } from '../requests.facade'
 
+export enum ILNESS_OPTIONS {
+  'None' = 'Nu are',
+  'Has symptoms' = 'Are simptome'
+}
+
+export enum CUSTOMER_NEEDS {
+  'Food' = 'Produse Alimentare',
+  'Drugs' = 'Medicamente',
+  'Transport' = 'Transport Persoana',
+  'Payment' = 'Achitare Facturi'
+}
+
 @Component({
   templateUrl: './request-details.component.html',
   styleUrls: ['./request-details.component.scss'],
@@ -11,38 +23,16 @@ import { RequestsFacade } from '../requests.facade'
 export class RequestDetailsComponent implements OnInit, OnDestroy {
   form: FormGroup
   zones: Array<string> = Object.keys(ZONES).filter((key) => isNaN(+key))
-  ilnessOptions: any = [
-    {
-      label: 'None',
-      value: 'Nu are'
-    },
-    {
-      label: 'Has symptoms',
-      value: 'Are simptome'
-    }
-  ]
-  needs: any = [
-    {
-      label: 'Food',
-      value: 'Produse Alimentare'
-    },
-    {
-      label: 'Drugs',
-      value: 'Medicamente'
-    },
-    {
-      label: 'Transport',
-      value: 'Transport Persoana'
-    },
-    {
-      label: 'Payment',
-      value: 'Achitare Facturi'
-    }
-  ]
+  needs: Array<string> = Object.values(CUSTOMER_NEEDS).filter((key) => isNaN(+key))
+  ilnessOptions: Array<string> = Object.values(ILNESS_OPTIONS).filter((key) => isNaN(+key))
   specialConditions = SPECIAL_CONDITIONS
   constructor(private requestsFacade: RequestsFacade) {}
 
-  onSubmit(ev: Event) {}
+  onSubmit(ev: Event) {
+    this.form.get('need').setValue(this.getEnumKeyByEnumValue(CUSTOMER_NEEDS, this.form.get('need').value))
+    this.form.get('ilness').setValue(this.getEnumKeyByEnumValue(ILNESS_OPTIONS, this.form.get('ilness').value))
+    console.log(this.form.get('urgent').value)
+  }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -64,8 +54,14 @@ export class RequestDetailsComponent implements OnInit, OnDestroy {
       ilness: new FormControl('', [Validators.required]),
       need: new FormControl('', [Validators.required]),
       comments: new FormControl(''),
-      password: new FormControl(null, [Validators.required])
+      password: new FormControl(null, [Validators.required]),
+      urgent: new FormControl(false, [Validators.required])
     })
   }
   ngOnDestroy() {}
+
+  getEnumKeyByEnumValue(myEnum, enumValue) {
+    let keys = Object.keys(myEnum).filter((x) => myEnum[x] == enumValue)
+    return keys.length > 0 ? keys[0] : null
+  }
 }
