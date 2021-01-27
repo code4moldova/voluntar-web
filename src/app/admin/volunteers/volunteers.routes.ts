2192881a790ef51404e686a05f653150139fe1a7
Dan Percic <danpercic86@gmail.com>
2021-01-27 20:53:55 +0200

    remove bad imports

--- src/app/admin/volunteers/volunteers.routes.ts
+++ src/app/admin/volunteers/volunteers.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { VolunteersDetailsComponent } from './volunteers-details/volunteers-details.component';
 import { VolunteersListComponent } from './volunteers-list/volunteers-list.component';
-import { UsersEditComponent } from '@users/users-edit/users-edit.component';
-import { UserResolverService } from '@users/shared/user-resolver.service';
-import { VolunteersEditComponent } from '@volunteers/volunteers-edit/volunteers-edit.component';
 import { VolunteerResolverService } from '@volunteers/shared/volunteer-resolver.service';
 
 export const volunteersRoutes: Routes = [
