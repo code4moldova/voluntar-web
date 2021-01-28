a3a5621eb3dfcac40d97c45258aec22a7c8baad3
Dan Percic <danpercic86@gmail.com>
2021-01-28 14:43:34 +0200

    change deprecated roles variable

--- src/app/admin/volunteers/volunteers-list/volunteers-list.component.ts
+++ src/app/admin/volunteers/volunteers-list/volunteers-list.component.ts
@@ -14,7 +14,7 @@ import { map, take, takeUntil } from 'rxjs/operators';
 import { saveVolunteerSuccessAction } from '../volunteers.actions';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
-import { KIV_ZONES, VolunteerRole, zones } from '@shared/constants';
+import { VolunteerRole, zones } from '@shared/constants';
 import { FilterObservableSelectColumns } from '@shared/filter/filter.types';
 import { VolunteersCreateComponent } from '../volunteers-create/volunteers-create.component';
 import { getOffersTagsAction } from '@shared/tags/tags.actions';
@@ -87,7 +87,7 @@ export class VolunteersListComponent implements OnInit {
       {
         name: 'Sector',
         value: 'zone_address',
-        array: of(KIV_ZONES),
+        array: of(zones),
       },
     ];
   }
