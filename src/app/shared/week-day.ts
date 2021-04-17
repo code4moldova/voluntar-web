2d4f10926cecbdb0254632c395e47f480a9ee850
Andrew Luca <thendrluca@gmail.com>
2021-04-17 15:50:35 +0300

    fix(volunteers): make monday first day of week

--- src/app/shared/week-day.ts
+++ src/app/shared/week-day.ts
@@ -2,13 +2,34 @@
  * Do not change the order of days, somewhere is dependent on Date.getDay()
  */
 export enum WeekDay {
-  sunday = 'sunday',
   monday = 'monday',
   tuesday = 'tuesday',
   wednesday = 'wednesday',
   thursday = 'thursday',
   friday = 'friday',
   saturday = 'saturday',
+  sunday = 'sunday',
 }
 
-export const weekDays = Object.values(WeekDay);
+/**
+ * We create the array manually because we can't rely on {@link ObjectConstructor.values} order
+ */
+export const weekDays = [
+  WeekDay.monday,
+  WeekDay.tuesday,
+  WeekDay.wednesday,
+  WeekDay.thursday,
+  WeekDay.friday,
+  WeekDay.saturday,
+  WeekDay.sunday,
+];
+
+/**
+ * In some places we use {@link DateConstructor.getDay} that assumes that Sunday is first
+ * The getDay() method returns the day of the week for the specified date according to local time, where 0 represents Sunday.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDay
+ */
+export const weekDaysSundayFirst = [
+  WeekDay.sunday,
+  ...weekDays.filter((wd) => wd !== WeekDay.sunday),
+];
